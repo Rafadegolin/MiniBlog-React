c295e5f0a6e6632c92314b85e2e21c9f3e538663
Rafael Degolin da Silva <103646841+Rafadegolin@users.noreply.github.com>
2024-02-26 18:52:38 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <h3>Escreva sobre o que você tem interesse!</h3>
-        <p>Mini Blog &copy; 2023</p>
+        <p>Mini Blog - Rafael Degolin &copy; 2023</p>
     </footer>
   );
 };
